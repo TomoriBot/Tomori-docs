fe923daecb8af2d96bebe75e4a0f3adbf8bed1ec
Michael <26341323+TomoriBot@users.noreply.github.com>
2019-11-01 04:49:35 +0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -31,10 +31,7 @@ release = '5.3.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'rinoh.frontend.sphinx',
-    'recommonmark'
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
